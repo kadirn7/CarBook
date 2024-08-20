a794522e0123e52b16377093e09cb887265bd67e
Kadir Paşaoğlu <92451463+kadirn7@users.noreply.github.com>
2024-08-20 23:40:04 +0300

    View Component Added project

---
+++ Frontend/CarBook.Web.UI/ViewComponents/UILayoutComponents/_MainCoverUILayoutComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.Web.UI.ViewComponents.UILayoutComponents
+{
+    public class _MainCoverUILayoutComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
