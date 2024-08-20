26fc6c99f40afb3630fe86b2271edf3796d5dfed
Kadir Paşaoğlu <92451463+kadirn7@users.noreply.github.com>
2024-08-21 00:46:11 +0300

    ++

--- Frontend/CarBook.Web.UI/ViewComponents/AboutViewComponents/_AboutUsComponentPartial.cs
+++ Frontend/CarBook.Web.UI/ViewComponents/AboutViewComponents/_AboutUsComponentPartial.cs
@@ -5,22 +5,22 @@ namespace CarBook.Web.UI.ViewComponents.AboutViewComponents
 {
     public class _AboutUsComponentPartial:ViewComponent
     {
-        IHttpClientFactory _httpClientFactory;
+        //IHttpClientFactory _httpClientFactory;
 
-        public _AboutUsComponentPartial(IHttpClientFactory httpClientFactory)
-        {
-            _httpClientFactory = httpClientFactory;
-        }
+        //public _AboutUsComponentPartial(IHttpClientFactory httpClientFactory)
+        //{
+        //    _httpClientFactory = httpClientFactory;
+        //}
 
         public async Task<IViewComponentResult> InvokeAsync() 
         {  
-            var client=_httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync("https://localhost:7234/api/Abouts");
-            if(responseMessage.IsSuccessStatusCode)
-            {
-                var jsonData= await responseMessage.Content.ReadAsStringAsync();
-                var values = JsonConvert.DeserializeObject<List<>>(jsonData);
-            }
+            //var client=_httpClientFactory.CreateClient();
+            //var responseMessage = await client.GetAsync("https://localhost:7234/api/Abouts");
+            //if(responseMessage.IsSuccessStatusCode)
+            //{
+            //    var jsonData= await responseMessage.Content.ReadAsStringAsync();
+            //    var values = JsonConvert.DeserializeObject<List<>>(jsonData);
+            //}
             return View(); 
         }
 
